c42e8b56315d8490c0eac442db842434d24f9f0a
Daniils Firgers <dfirger@gmail.com>
2024-07-02 22:26:15 +0300

    now can select city and district + parsing

--- src/telegram/mod.rs
+++ src/telegram/mod.rs
@@ -22,9 +22,9 @@ type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
 pub enum State {
     #[default]
     Start,
-    ReceiveFullName,
+    ReceiveCityName,
     ReceiveAge {
-        full_name: String,
+        city_name: String,
     },
     ReceiveLocation {
         full_name: String,
@@ -32,7 +32,10 @@ pub enum State {
     },
 }
 #[derive(BotCommands, Clone)]
-#[command(rename = "lowercase", description = "These commands are supported:")]
+#[command(
+    rename_rule = "lowercase",
+    description = "These commands are supported:"
+)]
 enum Command {
     #[command(description = "Start the dialogue.")]
     Start,
@@ -70,8 +73,8 @@ impl FlatsBotTelegram {
             Update::filter_message()
                 .enter_dialogue::<Message, InMemStorage<State>, State>()
                 .branch(dptree::case![State::Start].endpoint(Self::start))
-                .branch(dptree::case![State::ReceiveFullName].endpoint(Self::receive_full_name))
-                .branch(dptree::case![State::ReceiveAge { full_name }].endpoint(Self::receive_age))
+                .branch(dptree::case![State::ReceiveCityName].endpoint(Self::recieve_city_name))
+                .branch(dptree::case![State::ReceiveAge { city_name }].endpoint(Self::receive_age))
                 .branch(
                     dptree::case![State::ReceiveLocation { full_name, age }]
                         .endpoint(Self::receive_location),
@@ -106,26 +109,47 @@ impl FlatsBotTelegram {
         let greeting_message = format!("Let's start! Please select a city: \n\n{}", cities);
         println!("greeting_message: {:?}", greeting_message);
         bot.send_message(msg.chat.id, greeting_message).await?;
-        dialogue.update(State::ReceiveFullName).await?;
+        dialogue.update(State::ReceiveCityName).await?;
         println!("updated state");
         Ok(())
     }
 
-    async fn receive_full_name(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
-        println!("receive_full_name {:?}, dialogue: {:?}", msg, dialogue);
-        match msg.text() {
-            Some(text) => {
-                bot.send_message(msg.chat.id, "How old are you?").await?;
-                dialogue
-                    .update(State::ReceiveAge {
-                        full_name: text.into(),
-                    })
-                    .await?;
-            }
-            None => {
-                bot.send_message(msg.chat.id, "Send me plain text.").await?;
-            }
-        }
+    async fn recieve_city_name(
+        dependencies: Arc<BotDependencies>,
+        bot: Bot,
+        dialogue: MyDialogue,
+        msg: Message,
+    ) -> HandlerResult {
+        let Some(city_name): Option<&str> = msg.text() else {
+            bot.send_message(msg.chat.id, "Message should be a plain text").await?;
+            return Ok(())
+        };
+        let flats_parser = dependencies.flats_parser.lock().await;
+        let city_info = flats_parser
+            .cities
+            .iter()
+            .find(|city| city.name.eq(city_name));
+        let Some(city_info) = city_info else {
+            bot.send_message(msg.chat.id, format!("City '{}' name is invalid. Please try again!", city_name)).await?;
+            return Ok(())
+        };
+        let mut districts = city_info
+            .districts
+            .iter()
+            .map(|district| format!("• {}", district.name))
+            .collect::<Vec<_>>();
+        districts.sort();
+        let districts = districts.join("\n");
+        bot.send_message(
+            msg.chat.id,
+            format!("Please select a district: \n\n{}", districts),
+        )
+        .await?;
+        dialogue
+            .update(State::ReceiveAge {
+                city_name: city_name.into(),
+            })
+            .await?;
 
         Ok(())
     }
